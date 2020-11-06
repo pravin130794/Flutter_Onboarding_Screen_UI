f474da7a708bc70048350c4458df4b67ae9fda02
pravin130794 <pravin130794@gmail.com>
2020-11-06 21:28:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Flutter_Onboarding_Screen_UI
+ 
---
+++ android/app/src/main/kotlin/com/example/Flutter_Onboarding_Screen_UI/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Flutter_Onboarding_Screen_UI
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
